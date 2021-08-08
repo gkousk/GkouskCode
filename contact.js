b5de9955bd56daae84a83bde2d618afd13b028b0
gkousk <tp4399@edu.teicrete.gr>
2021-08-08 15:52:39 +0300

    Update contact form

--- contact.js
+++ contact.js
@@ -5,12 +5,17 @@ function sendMail() {
         mail:document.getElementById("mail").value,
         message:document.getElementById("msg").value,
     };
+    if(tempParams.name!="" && tempParams.mail!="" && tempParams.message!=""){
+        emailjs.send('service_yn8zqqj','template_n9g2r98',tempParams)
+        .then(function(res){
+            if(res.status==200){
+                r.innerHTML='Email sent successfully';
+                r.style.color="green";
+            }
+        })
+    }else{
+        r.style.color="red";
+        r.innerHTML='Email cant send';
+    }
     
-    emailjs.send('service_yn8zqqj','template_n9g2r98',tempParams)
-    .then(function(res){
-        if(res.status==200){
-            r.innerHTML='Email sent successfully';
-        }
-        
-    })
 }
