5287905b61351a9b7f29695419164102a1edcce1
gkousk <tp4399@edu.teicrete.gr>
2021-08-09 00:03:08 +0300

    Added validation to the contact

--- contact.js
+++ contact.js
@@ -5,17 +5,38 @@ function sendMail() {
         mail:document.getElementById("mail").value,
         message:document.getElementById("msg").value,
     };
-    if(tempParams.name!="" && tempParams.mail!="" && tempParams.message!=""){
+    if(tempParams.name == ""){
+        console.log("1")
+        r.innerHTML="Full Name is not valid";
+        r.style.color="red";
+    }
+    else if(!validateEmail(tempParams.mail)){
+        console.log("2");
+        r.innerHTML="Email is not valid";
+        r.style.color="red";
+    }
+    else if(tempParams.message == ""){
+        console.log(3)
+        r.innerHTML="Message is not valid";
+        r.style.color="red";
+    }
+    else{
         emailjs.send('service_yn8zqqj','template_n9g2r98',tempParams)
         .then(function(res){
             if(res.status==200){
-                r.innerHTML='Email sent successfully';
+                r.innerHTML='Mail sent successfully';
+                r.style.color="green";
+            }
+            else{
+                r.innerHTML='There is a problem sending mail';
                 r.style.color="green";
             }
         })
-    }else{
-        r.style.color="red";
-        r.innerHTML='Email cant send';
-    }
-    
+    }    
+}
+
+function validateEmail(email) 
+{
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
